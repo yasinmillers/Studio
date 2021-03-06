c54f64a70d603e3def57384b2d647cf76f0e1d35
yasinmillers <54667195+yasinmillers@users.noreply.github.com>
2021-03-06 05:04:40 -0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.assjava;
+import java.util.Arrays;
+
+public class MainActivity{
+    public static void main(String[]arg){
+        String[] Units ={"Mobile programming CSC3115","artificial intelligent CSC3116","automata CSC3117","DataC Communication and Networks CSC3112","Mobile progomming CSC3116","CSC3113 Entrepreneurship"};
+        for( int i=0;i < Units.length;i++){
+            System.out.println(Units[i]);
+        }
+
+    }
+}
