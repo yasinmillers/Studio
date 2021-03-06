f8ecdb0dde07b421e430977ca51e51f09de6016c
yasinmillers <54667195+yasinmillers@users.noreply.github.com>
2021-03-06 05:39:58 -0800

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.mykotlin
+
+fun main(args: Array<String>){
+
+    var course: Array<String> = arrayOf("CSC3117 Automata Complexity and Computability,CSC3115 MobileProgramming, CSC3113 Entrepreneurship, CSC3104 Computer Networks and Security,CSC3 Artificial Intelligence, CSC3112 Data Communications and Networks, ")
+
+    course.forEach{System.out.println(it)}
+
+}
